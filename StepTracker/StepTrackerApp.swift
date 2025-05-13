6c36ff27ef9891e522dccdcf6271a4f16ef089ee
Ryandj11 <ryanjohnson1105@gmail.com>
2025-05-13 13:37:44 -0700

    first commit

---
+++ StepTracker/StepTrackerApp.swift
@@ -0,0 +1,29 @@
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Ryan Johnson on 5/13/25.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct StepTrackerApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
